ebe8413c78fdd56181889640f4a743c36916c781
seanquiambao <paparoxii03@gmail.com>
2026-07-08 19:21:54 -0700

    implemented dynamic dialogue manager with customizable backgrounds

--- dialogue/scenes/common/base/dialogue_base.gd
+++ dialogue/scenes/common/base/dialogue_base.gd
@@ -1,14 +1,13 @@
-extends Node3D
+extends DialogicBackground
 
 class_name DialogueCharacter
 @onready var dialogue_camera: Camera3D = $DialogueCamera
 @onready var characters: Node = $Characters
 
-@export var timeline: DialogicTimeline
+var timeline: DialogicTimeline
 
 func _ready() -> void:
 	Dialogic.Portraits.character_joined.connect(_on_character_joined)
-	Dialogic.start(timeline)
 
 func _on_character_joined(info: Dictionary) -> void:
 	var character: DialogicCharacter = info.character
--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -3,17 +3,34 @@ extends Control
 @export var timelines: Array[DialogicTimeline]
 @onready var option_button: OptionButton = $CanvasLayer/VBoxContainer/OptionButton
 @onready var error: Label = $CanvasLayer/VBoxContainer/Error
+@onready var sub_viewport: SubViewport = %SubViewport
 
+var current_viewport_instance
+var current_timeline: DialogicTimeline
 func _ready() -> void:
 	for timeline in timelines:
 		var timeline_name: String = timeline.resource_path.split("/")[-1].replace(".dtl", "")
 		option_button.add_item(timeline_name);
+	Dialogic.Backgrounds.background_changed.connect(_on_background_changed)
+	Dialogic.timeline_ended.connect(_on_timeline_ended)
 
 func _on_button_pressed() -> void:
 	var selected_id = option_button.get_selected_id()
 	if selected_id == -1:
 		error.text = "You have not selected a timeline!"
 		return
-	
-	print(timelines[selected_id])
-	
+	current_timeline = timelines[selected_id]
+	Dialogic.start(timelines[selected_id])
+
+func _on_timeline_ended() -> void:
+	sub_viewport.remove_child(current_viewport_instance)
+	current_viewport_instance.queue_free()
+	current_timeline = null
+
+func _on_background_changed(info: Dictionary) -> void:
+	if not info.scene:
+		return
+	var scene: PackedScene = await load(info.scene)
+	current_viewport_instance = scene.instantiate()
+	current_viewport_instance.timeline = current_timeline
+	sub_viewport.add_child(current_viewport_instance)
