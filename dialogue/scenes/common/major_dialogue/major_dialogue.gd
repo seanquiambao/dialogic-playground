919c58e828eb5d00cc0770655db5fdf5534337fb
seanquiambao <paparoxii03@gmail.com>
2026-07-09 21:03:36 -0700

    cleanup + file organization

--- dialogue/scenes/common/major_dialogue/major_dialogue.gd
+++ dialogue/scenes/common/major_dialogue/major_dialogue.gd
@@ -1,7 +1,7 @@
 extends Control
 @onready var character_subviewport: SubViewport = $CharacterContainer/CharacterSubviewport
 @onready var protagonist_subviewport: SubViewport = $ProtagonistContainer/ProtagonistSubviewport
-@onready var protagonist_scene = preload("res://dialogue/scenes/common/characters/protagonist_character.tscn")
+@onready var protagonist_scene = preload("res://dialogue/scenes/common/major_dialogue/characters/protagonist_character.tscn")
 
 var current_timeline: DialogicTimeline
 
@@ -10,15 +10,23 @@ var protagonist_scene_instance
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	clear_viewports()
 	Dialogic.Backgrounds.background_changed.connect(_on_background_changed)
 	Dialogic.timeline_ended.connect(_on_timeline_ended)
 
-func _on_timeline_ended() -> void:
-	character_subviewport.remove_child(character_scene_instance)
-	protagonist_subviewport.remove_child(protagonist_scene_instance)
-	protagonist_scene_instance.queue_free()
-	character_scene_instance.queue_free()
+func clear_viewports() -> void:
+	for child in character_subviewport.get_children():
+		child.free()
+	for child in protagonist_subviewport.get_children():
+		child.free()
+	if protagonist_scene_instance:
+		protagonist_scene_instance.queue_free()
+	if character_scene_instance:
+		character_scene_instance.queue_free()
 	current_timeline = null
+
+func _on_timeline_ended() -> void:
+	clear_viewports()
 	queue_free()
 
 func setup_character_view(info: Dictionary) -> void:
@@ -30,13 +38,10 @@ func setup_character_view(info: Dictionary) -> void:
 
 func setup_protagonist_view(info: Dictionary) -> void:
 	var image_path = info.argument
-	if not image_path:
-		return
-	if not ResourceLoader.exists(image_path):
-		return
-	var image_texture = ResourceLoader.load(image_path)
 	protagonist_scene_instance = protagonist_scene.instantiate()
-	protagonist_scene_instance.background_texture = image_texture
+	if image_path and ResourceLoader.exists(image_path):
+		var image_texture = ResourceLoader.load(image_path)
+		protagonist_scene_instance.background_texture = image_texture
 	protagonist_subviewport.add_child(protagonist_scene_instance)
 
 func _on_background_changed(info: Dictionary) -> void:
--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -3,7 +3,7 @@ extends Control
 @export var timelines: Array[DialogicTimeline]
 @onready var option_button: OptionButton = $CanvasLayer/VBoxContainer/OptionButton
 @onready var error: Label = $CanvasLayer/VBoxContainer/Error
-@onready var major_style = preload("res://dialogue/scenes/common/major_style/major_style.tscn")
+@onready var major_dialogue = preload("res://dialogue/scenes/common/major_dialogue/major_dialogue.tscn")
 
 func _ready() -> void:
 	for timeline in timelines:
@@ -16,7 +16,7 @@ func _on_button_pressed() -> void:
 	if selected_id == -1:
 		error.text = "You have not selected a timeline!"
 		return
-	var major_style_instance = major_style.instantiate()
-	major_style_instance.current_timeline = timelines[selected_id]
-	add_child(major_style_instance)
+	var major_dialogue_instance = major_dialogue.instantiate()
+	major_dialogue_instance.current_timeline = timelines[selected_id]
+	add_child(major_dialogue_instance)
 	Dialogic.start(timelines[selected_id])
